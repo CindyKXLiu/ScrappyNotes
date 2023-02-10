af144a8303163ed27b32213e94a7dc316da56934
Rerenah <60529747+Rerenah@users.noreply.github.com>
2023-02-10 11:35:24 -0500

    Added test directories to all projects

---
+++ shared/src/test/kotlin/MainTest.kt
@@ -0,0 +1,11 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+internal class MainTest {
+    /* private val testSample: Sample = Sample()
+    @Test
+    fun testSum() {
+        val expected = 42
+        assertEquals(expected, testSample.sum(40, 2))
+    }
+     */
+}
