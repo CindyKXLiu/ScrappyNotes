9c504d3967e8077510ecc538d5e34404f3e37638
Rerenah <60529747+Rerenah@users.noreply.github.com>
2023-02-16 01:49:00 -0500

    Added Search and reorganized lay out

--- application/src/main/kotlin/cs346/application/Main.kt
+++ application/src/main/kotlin/cs346/application/Main.kt
@@ -13,6 +13,8 @@ import javafx.scene.input.KeyCode
 import javafx.scene.input.MouseButton
 import javafx.scene.layout.BorderPane
 import javafx.scene.layout.HBox
+import javafx.scene.layout.Priority
+import javafx.scene.layout.VBox
 import javafx.stage.Stage
 import java.util.*
 
@@ -25,107 +27,63 @@ class Main : Application() {
     private val layout = BorderPane()
     private val controller = Controller()
     override fun start(stage: Stage) {
-        val twotop = BorderPane()
-
-        // top: menubar
+        /**
+         * Set up all drop down menus and functionality for top most bar
+         */
         val menuBar = MenuBar()
 
         // FILE menubar manipulations /////////////////////////////////////////////////////////
         val fileMenu = Menu("File")
         val fileQuit = MenuItem("Quit")
-        fileQuit.setOnAction { event ->
-            stage.close()
-        }
+        fileQuit.setOnAction { event -> stage.close() }
 
-        // VIEW menubar manipulations /////////////////////////////////////////////////////////
-        val viewMenu = Menu("View")
-        val viewNext = MenuItem("Next")
-        viewNext.setOnAction { event ->
-            // enterDirectory()
-        }
-        val viewBack = MenuItem("Prev")
-        viewBack.setOnAction { event ->
-           // backDirectory()
-        }
+        val newNote = MenuItem("New")
+        newNote.setOnAction { event -> createNote() }
+
+        val deleteNote = MenuItem("Delete")
+        deleteNote.setOnAction { event -> deleteSelectedNote() }
+
+        fileMenu.items.addAll(fileQuit, newNote, deleteNote)
+        menuBar.menus.add(fileMenu)
 
         // ACTIONS menubar manipulations ////////////////////////////////////////////////////////
         val actionsMenu = Menu("Actions")
-        val actionsDelete = MenuItem("Delete")
-        actionsDelete.setOnAction { event ->
-            deleteSelectedNote()
-        }
+
         val actionsRename = MenuItem("Rename")
-        actionsRename.setOnAction { event ->
-            // renameSelected()
-        }
+        actionsRename.setOnAction { event -> renameSelectedNote() }
         val actionsMove = MenuItem("Move")
         actionsMove.setOnAction { event ->
             // moveSelected()
         }
 
-        // OPTIONS menubar manipulations ///////////////////////////////////////////////////////////
-        val optionsMenu = Menu("Options")
-        val optionsHide = CheckMenuItem("Show Hidden")
-        optionsHide.setOnAction { event ->
-            /*displayHidden = !displayHidden
-            updateTree()*/
-        }
-
-        fileMenu.items.add(fileQuit)
-        menuBar.menus.add(fileMenu)
-
-        viewMenu.items.add(viewNext)
-        viewMenu.items.add(viewBack)
-        menuBar.menus.add(viewMenu)
-
-        actionsMenu.items.add(actionsDelete)
         actionsMenu.items.add(actionsRename)
         actionsMenu.items.add(actionsMove)
         menuBar.menus.add(actionsMenu)
 
+        // OPTIONS menubar manipulations ///////////////////////////////////////////////////////////
+        val optionsMenu = Menu("Options")
+        val optionsHide = CheckMenuItem("Select Theme")
+        optionsHide.setOnAction { event -> }
+
         optionsMenu.items.add(optionsHide)
         menuBar.menus.add(optionsMenu)
 
-        ///////////////////////////////////////////////////////////////////////////////////////////////
-        val secondBar = HBox()
-
-        val createButton = Button("New Note")
-        createButton.setOnAction { actionEvent: ActionEvent ->
-            createNote()
-        }
-
-        val saveButton = Button("Save Note")
-        saveButton.setOnAction { actionEvent: ActionEvent ->
-            saveSelectedNote()
-        }
-
-        val deleteButton = Button("Delete")
-        deleteButton.setOnAction { event -> deleteSelectedNote() }
-
-        val renameButton = Button("Rename")
-        renameButton.setOnAction {}
-
-        val moveButton = Button("Move")
-        moveButton.setOnAction {}
-
-        secondBar.spacing = 7.0
-        secondBar.padding = Insets(3.0, 5.0, 3.0, 5.0)
-        secondBar.children.addAll(createButton, saveButton, deleteButton, renameButton, moveButton)
-        twotop.top = menuBar
-        twotop.bottom = secondBar
-
-
+        /**
+         * Set up Bottom Panel displaying last modification date of current file
+         */
         lastmodified.padding = Insets(3.0, 5.0, 3.0, 5.0)
         lastmodified.children.add(Label(""))
-        ////////////////////////////////////////////////////////////////////////////////////////////////
-        // left: list of notes
+
+        /**
+         * Set up for left side note list display
+         */
         updateNoteview(controller.getSortedNotesByModifiedDateAscending())
         noteview.selectionModel.selectedItemProperty().addListener { observableValue: ObservableValue<out String>, old_val: String?, new_val: String? ->
             val currIndex = noteview.selectionModel.selectedIndex
             if (currIndex != -1) displayNoteContents(notedata[currIndex])
+            textarea.disableProperty().set(false)
         }
 
-        // handle mouse clicked action
         noteview.setOnMouseClicked { event ->
             if (event.button.equals(MouseButton.PRIMARY)) {
                 if (event.clickCount == 1 && noteview.items.size != 0) {
@@ -134,6 +92,7 @@ class Main : Application() {
                 }
             }
         }
+
         noteview.setOnKeyPressed { event ->
             if (event.code == KeyCode.ENTER) {
                 val currIndex = noteview.selectionModel.selectedIndex
@@ -143,20 +102,59 @@ class Main : Application() {
             }
         }
 
-        // default center
-        textarea.focusTraversableProperty().set(false)
-        textarea.disableProperty().set(true)
+        /**
+         * Set up for left side search bar
+         */
+        val searchbox = TextField()
+        searchbox.promptText = "search"
+        searchbox.setOnKeyPressed { event ->
+            if (event.code == KeyCode.ENTER) {
+                searchNotes(searchbox.text)
+            }
+        }
 
-        // build the scene graph
-        layout.top = twotop
-        layout.left = noteview
-        layout.center = textarea
+        val leftside = VBox()
+        leftside.spacing = 0.0
+        VBox.setVgrow(noteview, Priority.ALWAYS)
+        leftside.children.addAll(searchbox, noteview)
+
+        /**
+         * Set up for focus area text and toolbar
+         */
+        val mainarea = VBox()
+
+        val texttools = HBox()
+        val boldButton = Button("Bold")
+        boldButton.setOnAction { actionEvent: ActionEvent -> }
+
+        val italicizeButton = Button("Italics")
+        italicizeButton.setOnAction { actionEvent: ActionEvent -> }
+
+        val underlineButton = Button("Underline")
+        underlineButton.setOnAction { }
+
+        val saveButton = Button("Save Note")
+        saveButton.setOnAction { actionEvent: ActionEvent -> saveSelectedNote() }
+
+        texttools.spacing = 7.0
+        texttools.padding = Insets(3.0, 5.0, 3.0, 5.0)
+        texttools.children.addAll(boldButton, italicizeButton, underlineButton, saveButton)
+        VBox.setVgrow(textarea, Priority.ALWAYS)
+        textarea.focusTraversableProperty().set(false)
+
+        mainarea.children.addAll(texttools, textarea)
+
+        /**
+         * Add all panels to scene and show
+         */
+        layout.top = menuBar
+        layout.left = leftside
+        layout.center = mainarea
         layout.bottom = lastmodified
 
-        // create and show the scene
         val scene = Scene(layout)
-        stage.minWidth = 50.0
-        stage.minHeight = 50.0
+        stage.minWidth = 400.0
+        stage.minHeight = 300.0
         stage.width = 800.0
         stage.height = 600.0
         stage.scene = scene
@@ -182,9 +180,14 @@ class Main : Application() {
                 }
             }
             noteview.items = newview
-            if (selectedNote != null && indexofnote != -1) {
-                noteview.scrollTo(indexofnote)
-                noteview.selectionModel.select(indexofnote)
+            if (selectedNote != null) {
+                if (indexofnote != -1) {
+                    noteview.scrollTo(indexofnote)
+                    noteview.selectionModel.select(indexofnote)
+                }
+            } else {
+                textarea.text = null
+                textarea.disableProperty().set(true)
             }
         }
     }
@@ -196,7 +199,6 @@ class Main : Application() {
         val result: Optional<String> = td.showAndWait()
         if (result.isPresent) {
             val newnote = controller.createNote(result.get(),"blank")
-            layout.center = textarea
             textarea.disableProperty().set(false)
             displayNoteContents(newnote)
             updateNoteview(controller.getSortedNotesByModifiedDateDescending(), newnote)
@@ -216,8 +218,11 @@ class Main : Application() {
             val result = alert.showAndWait()
             if (result.get() == ButtonType.OK) {
                 controller.deleteNote(notedata[currIndex].id)
-                layout.center = null
-                updateNoteview(controller.getSortedNotesByModifiedDateDescending())
+                textarea.text = null
+                textarea.disableProperty().set(true)
+                notedata.removeAt(noteview.selectionModel.selectedIndex)
+                noteview.items.removeAt(noteview.selectionModel.selectedIndex)
+                updateNoteview(notedata.toList())
             } else {
                 alert.close()
             }
@@ -225,7 +230,7 @@ class Main : Application() {
     }
 
     private fun displayNoteContents(note: Note) {
-        layout.center = textarea
+        textarea.disableProperty().set(false)
         textarea.text = note.content
     }
 
@@ -236,4 +241,31 @@ class Main : Application() {
             controller.editNoteContent(currNote.id, textarea.text)
         }
     }
+
+    private fun searchNotes(search : String) {
+        val notes = mutableListOf<Note>()
+        if (search.isNotEmpty()) {
+            notes.addAll(controller.getNotesByTitle(search))
+            notes.addAll(controller.getNotesByContent(search))
+            updateNoteview(notes)
+        } else {
+            updateNoteview(controller.getSortedNotesByModifiedDateDescending())
+        }
+    }
+
+    private fun renameSelectedNote() {
+        val currIndex = noteview.selectionModel.selectedIndex
+        if (currIndex != -1) {
+            val td = TextInputDialog(notedata[currIndex].title)
+            td.headerText = "Enter a new title for your note."
+            val result: Optional<String> = td.showAndWait()
+            if (result.isPresent) {
+                controller.editNoteTitle(notedata[currIndex].id, result.get())
+                textarea.disableProperty().set(false)
+                displayNoteContents(notedata[currIndex])
+                updateNoteview(controller.getSortedNotesByModifiedDateDescending(), notedata[currIndex])
+            }
+        }
+    }
+
 }
